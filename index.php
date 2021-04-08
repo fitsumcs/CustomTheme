fd0e1b3db8fda209ca148f31584037f5faa8e4fe
Fitsum A <fitsumcs@gmail.com>
2021-04-08 23:49:36 +0400

    Adding the loop to display the post contenet

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<h1>This is Test </h1>
+<?php 
+
+if(have_posts()){
+    while(have_posts()){
+        the_post()?>
+        <h1><?php the_title()?></h1>
+        <?php the_content()?>
+    <?php }
+     
+
+}
+  
+?>
+
