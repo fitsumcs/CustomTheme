ede532397eae6a6d3dc7550b4fe1b66f6662fd2b
Fitsum A <fitsumcs@gmail.com>
2021-04-08 23:51:36 +0400

    Adding the permalin to each post , which is a link to the post detail

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 if(have_posts()){
     while(have_posts()){
         the_post()?>
-        <h1><?php the_title()?></h1>
+        <h1> <a href="<?php the_permalink()?>"><?php the_title()?></a></h1>
         <?php the_content()?>
     <?php }
      
