36d3388b0a8dd31b14e2618f0245c26d69b5c6d8
Fitsum A <fitsumcs@gmail.com>
2021-04-09 00:13:17 +0400

    adding wp_head to make sure that the theme works properly with plugins

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo('name')?></title>
+    <?php wp_head() ?>
 </head>
 <body <?php body_class()?>>
 <header class="site-header">
