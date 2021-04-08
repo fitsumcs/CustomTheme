05f048861eb1b7df5eee7b59772b5819044c9008
Fitsum A <fitsumcs@gmail.com>
2021-04-09 00:10:27 +0400

    adding site header information using bloginfo method

--- header.php
+++ header.php
@@ -6,4 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo('name')?></title>
 </head>
-<body>
+<body <?php body_class()?>>
+<header class="site-header">
+<h1><?php bloginfo('name')?></h1>
+<h4><?php bloginfo('description')?></h4>
+</header>
