305fe8df21f9d5e50614cccde499a5b4702b2e7f
Fitsum A <fitsum@gmail.com>
2021-04-14 22:35:03 +0300

    adding bootstrap js link on footer

--- footer.php
+++ footer.php
@@ -4,5 +4,8 @@
 <p><?php bloginfo( 'name' ) ?> - Developed by Fitsum &copy;<?php echo Date('Y')?></p>
 </footer>
 <?php wp_footer() ?>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="<?php bloginfo('template_url')?>/assets/js/bootstrap.bundle.min.js" ></script>
+
 </body>
 </html>
