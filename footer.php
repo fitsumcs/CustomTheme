85c9df30d833809f97c036148a79cd48211b59cb
Fitsum A <fitsumcs@gmail.com>
2021-04-15 07:14:18 +0400

    adding few css and change on the footer

--- footer.php
+++ footer.php
@@ -1,9 +1,8 @@
 
 <hr>
 <footer class="site-footer">
-    <p><?php bloginfo( 'name' ) ?></p>
+<p><?php bloginfo( 'name' ) ?> - Developed by Fitsum &copy;<?php echo Date('Y')?></p>
 </footer>
-<p>Developed by Fitsum &copy;2021</p>
 <?php wp_footer() ?>
 </body>
 </html>
