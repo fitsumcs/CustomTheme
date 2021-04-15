dbbd44010a289c19f484a649a1424fbca8719ce5
Fitsum A <fitsumcs@gmail.com>
2021-04-15 06:09:15 +0400

    adding menu hook to register the two menu location

--- functions.php
+++ functions.php
@@ -7,5 +7,19 @@
 add_theme_support('menus');
 
 
+// Register Nav Menu 
+function wct_custom_new_menu() {
+    register_nav_menus(
+      array(
+        'top-menu' => __( 'Top Menu' ),
+        'footer-menu' => __( 'Footer Menu' )
+      )
+    );
+  }
+
+
+//   Add the hook to make the menu 
+add_action( 'init', 'wct_custom_new_menu' );
+
 
 ?>
