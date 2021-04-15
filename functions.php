229ea1c42dbb7618f343c4aa41f270ab41bde59e
Fitsum A <fitsumcs@gmail.com>
2021-04-15 06:00:02 +0400

    adding menu support

---
+++ functions.php
@@ -0,0 +1,11 @@
+<!-- File where there are some functions that add more functionality to themes, such as the logo, menus, color, thumbnails, scripts, and stylesheets;-->
+<?php
+
+
+
+// Add Menu Support
+add_theme_support('menus');
+
+
+
+?>
