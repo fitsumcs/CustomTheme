3da26d29af3259853cc38f68067e45f0fb6c0be3
Fitsum A <fitsumcs@gmail.com>
2021-04-22 23:59:32 +0300

    adding comment on the single page

--- single.php
+++ single.php
@@ -11,8 +11,16 @@ if(have_posts()){
         <?php 
         
         the_content();
+
+
+        // If comments are open or there is at least one comment, load up the comment template.
+	if ( comments_open() || get_comments_number() ) {
+		comments_template();
+	}
         
     }}?>
+
+
 </div>
 
 <?php
