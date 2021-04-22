4f24f35af1ac2efa676d6af068c7e55a24b72192
Fitsum A <fitsumcs@gmail.com>
2021-04-22 16:56:57 +0400

    few updates on css

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <!-- Bootstrap CSS -->
-      <!-- <link href="<?php bloginfo('template_url') ?>/assets/css/bootstrap.min.css" rel="stylesheet" > -->
+
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <title><?php bloginfo('name')?></title>
     <?php wp_head() ?>
@@ -18,9 +18,9 @@
 <h1>   <a href="<?php echo home_url()?>"><?php bloginfo('name')?></a>  </h1>
 <h4><?php bloginfo('description')?></h4>
 </header>
-
+</div>
 <nav class="navbar navbar-expand-md navbar-dark bg-dark" role="navigation">
-  <div class="container">    <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="container">   
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -39,5 +39,5 @@
     </div>
 </nav>
 
-</div>
+
 
