628276c7fee984a43a482b6ee6ee4032e7ad80f7
Fitsum A <fitsumcs@gmail.com>
2021-04-23 00:33:46 +0300

    adding template parts

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
 <?php 
 if(have_posts()){
     while(have_posts()){
-        the_post()?>
-        <h1> <a href="<?php the_permalink()?>"><?php the_title()?></a></h1>
-        <?php the_excerpt();
+        the_post();
+        get_template_part('contents/content');
+      
      }}?>
 </div>
 
--- single.php
+++ single.php
@@ -6,11 +6,9 @@ get_header();
 <?php
 if(have_posts()){
     while(have_posts()){
-        the_post()?>
-        <h1> <a href="<?php the_permalink()?>"><?php the_title()?></a></h1>
-        <?php 
-        
-        the_content();
+        the_post();
+
+        get_template_part('contents/content');
 
 
         // If comments are open or there is at least one comment, load up the comment template.
