b90a70dcdce4abc1abba8491a5c01b9464fb0ea8
Fitsum A <fitsumcs@gmail.com>
2021-04-22 14:41:14 +0400

    register navwalker

--- functions.php
+++ functions.php
@@ -42,4 +42,13 @@ function wct_loadStyle()
 }
 add_action( 'wp_enqueue_scripts', 'wct_loadStyle' );
 
+/**
+ * Register Custom Navigation Walker
+ */
+function register_navwalker(){
+	require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+add_action( 'after_setup_theme', 'register_navwalker' );
+
+
 ?>
